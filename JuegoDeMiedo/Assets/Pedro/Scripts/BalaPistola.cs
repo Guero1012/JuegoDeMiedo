5550249fcf74c889d8a60e0ded5512f23e134ad9
PeterGlz <piither7@gmail.com>
2019-05-30 18:58:33 -0500

    disparo

--- JuegoDeMiedo/Assets/Pedro/Scripts/BalaPistola.cs
+++ JuegoDeMiedo/Assets/Pedro/Scripts/BalaPistola.cs
@@ -5,17 +5,19 @@ using UnityEngine;
 public class BalaPistola : MonoBehaviour {
 
     Rigidbody rb;
-    float speed = 20.0f;
+    float speed = 1.0f;
 
     void Start()
     {
-        rb.GetComponent<Rigidbody>();
-        rb.velocity = transform.forward * speed;
+        rb = gameObject.GetComponent<Rigidbody>();
+        //rb.velocity = transform.forward * speed;
+        
     }
 
     void Update()
     {
         Destroy(gameObject, 3.0f);
+        rb.AddForce(transform.forward * speed, ForceMode.Impulse);
     }
 
     void OnTriggerEnter(Collider _col)
