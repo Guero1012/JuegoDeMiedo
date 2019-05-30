f4a85714f13d8538708d0783e28d97ff64886cd4
David Hdz <dave.hzad@gmail.com>
2019-05-29 20:38:00 -0500

    Celular

---
+++ JuegoDeMiedo/Assets/David/AppsManagement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AppsManagement : MonoBehaviour {
+
+    public GameObject Linterna;
+
+    public GameObject PanelLinterna;
+    public Button LintOn;
+    public Sprite[] LintSprites;
+
+    public GameObject PanelVida;
+
+    AudioSource myAudio;
+    public AudioClip click;
+
+	// Use this for initialization
+	void Start () {
+        myAudio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    public void AppLinterna()
+    {
+        myAudio.PlayOneShot(click);
+        PanelLinterna.SetActive(true);
+    }
+
+    public void AppVida()
+    {
+        myAudio.PlayOneShot(click);
+        PanelVida.SetActive(true);
+    }
+
+    public void BotonLinterna()
+    {
+        myAudio.PlayOneShot(click);
+
+        if (Linterna.activeSelf)
+        {
+            LintOn.image.sprite = LintSprites[0];
+            Linterna.SetActive(false);
+        } else
+        {
+            LintOn.image.sprite = LintSprites[1];
+            Linterna.SetActive(true);
+        }
+    }
+
+    public void CloseApp()
+    {
+        myAudio.PlayOneShot(click);
+
+        if (PanelLinterna.activeSelf)
+        {
+            PanelLinterna.SetActive(false);
+            LintOn.image.sprite = LintSprites[0];
+
+        }
+
+        if (PanelVida.activeSelf)
+        {
+            PanelVida.SetActive(false);
+        }
+
+
+    }
+}
