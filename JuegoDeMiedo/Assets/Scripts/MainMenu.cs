3cc4610f8d0e4e06173ece54fb31d6dc488c7e9c
Alex <alexppcdsalvc@hotmail.com>
2019-12-03 07:09:36 -0600

    MainMenu
    Listo

--- JuegoDeMiedo/Assets/Scripts/MainMenu.cs
+++ JuegoDeMiedo/Assets/Scripts/MainMenu.cs
@@ -10,6 +10,8 @@ public class MainMenu : MonoBehaviour
     public GameObject[] botones;
     public GameObject[] imagenes;
     public GameObject[] titulos;
+    public GameObject[] sliders;
+    public GameObject[] dropDown;
     // Start is called before the first frame update
     void Start()
     {
@@ -68,6 +70,39 @@ public class MainMenu : MonoBehaviour
         titulos[2].SetActive(false);
     }
 
+    public void Regresar3()
+    {
+        botones[0].SetActive(true);
+        botones[1].SetActive(true);
+        botones[2].SetActive(true);
+        botones[3].SetActive(true);
+        botones[4].SetActive(false);
+        botones[5].SetActive(false);
+        botones[6].SetActive(false);
+        botones[7].SetActive(false);
+        botones[8].SetActive(false);
+
+        imagenes[0].SetActive(true);
+        imagenes[1].SetActive(false);
+
+        titulos[0].SetActive(true);
+        titulos[1].SetActive(false);
+        titulos[2].SetActive(false);
+        titulos[3].SetActive(false);
+        titulos[4].SetActive(false);
+        titulos[5].SetActive(false);
+        titulos[6].SetActive(false);
+        titulos[7].SetActive(false);
+        titulos[8].SetActive(false);
+
+        sliders[0].SetActive(false);
+        sliders[1].SetActive(false);
+        sliders[2].SetActive(false);
+
+        dropDown[0].SetActive(false);
+        dropDown[1].SetActive(false);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene("PruebasFinal_Dentro");
@@ -80,8 +115,36 @@ public class MainMenu : MonoBehaviour
 
     public void Options()
     {
+        botones[0].SetActive(false);
+        botones[1].SetActive(false);
+        botones[2].SetActive(false);
+        botones[3].SetActive(false);
+        botones[4].SetActive(false);
+        botones[5].SetActive(false);
+        botones[6].SetActive(false);
+        botones[7].SetActive(false);
+        botones[8].SetActive(true);
 
-        
+        imagenes[0].SetActive(true);
+        imagenes[1].SetActive(false);
+
+        titulos[0].SetActive(false);
+        titulos[1].SetActive(false);
+        titulos[2].SetActive(true);
+        titulos[3].SetActive(true);
+        titulos[4].SetActive(true);
+        titulos[5].SetActive(true);
+        titulos[6].SetActive(true);
+        titulos[7].SetActive(true);
+        titulos[8].SetActive(true);
+
+        sliders[0].SetActive(true);
+        sliders[1].SetActive(true);
+        sliders[2].SetActive(true);
+
+        dropDown[0].SetActive(true);
+        dropDown[1].SetActive(true);
+
     }
 
     public void Controls()
